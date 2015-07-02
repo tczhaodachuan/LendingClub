d2f8b6bbdbc789a88c201510cf861d2aa5ffcbca
tczhaodachuan <tczhaodachuan@gmail.com>
2015-07-02 00:23:15 -0400

    CommonData

--- CommonData/src/main/java/org.p2p.lending.club.api.data/Edge.java
+++ CommonData/src/main/java/org.p2p.lending.club.api.data/Edge.java
@@ -4,4 +4,11 @@ package org.p2p.lending.club.api.data;
  * Created by tczhaodachuan on 7/1/2015.
  */
 public class Edge {
+    private final Vertex v;
+    private final Vertex m;
+
+    public Edge(Vertex v, Vertex m) {
+        this.v = v;
+        this.m = m;
+    }
 }
--- CommonData/src/main/java/org.p2p.lending.club.api.data/Graph.java
+++ CommonData/src/main/java/org.p2p.lending.club.api.data/Graph.java
@@ -6,4 +6,11 @@ import java.util.Vector;
  * Created by tczhaodachuan on 7/1/2015.
  */
 public class Graph {
+    private final Vector<Vertex> vertexes;
+    private final Vector<Edge> edges;
+
+    public Graph() {
+        vertexes = new Vector<>();
+        edges = new Vector<>();
+    }
 }
